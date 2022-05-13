6c882473ef351bc36bd7692784c5545a09f60c11
Henry Lee <henryleeworld@gmail.com>
2022-05-14 00:48:10 +0800

    feat:初始化

---
+++ resources/lang/vendor/livewire-powergrid/zh_TW/datatable.php
@@ -0,0 +1,45 @@
+<?php
+
+return [
+    'buttons' => [
+        'filter'          => '過濾',
+    ],
+    'labels' => [
+        'action'           => '操作',
+        'results_per_page' => '每頁記錄',
+        'clear_filter'     => '清除過濾',
+        'no_data'          => '沒有找到記錄',
+        'all'              => '全部',
+        'selected'         => '已選擇',
+    ],
+    'placeholders' => [
+        'search' => '搜尋...',
+        'select' => '選擇一個區間',
+    ],
+    'pagination' => [
+        'showing' => '顯示',
+        'to'      => '至',
+        'of'      => '於',
+        'results' => '結果',
+        'all'     => '全部',
+    ],
+    'multi_select' => [
+        'select' => '選擇',
+        'all'    => '全部',
+    ],
+    'select' => [
+        'select' => '選擇',
+        'all'    => '全部'
+    ],
+    'boolean_filter' => [
+        'all' => '全部',
+    ],
+    'input_text_options' => [
+        'is'           => '是',
+        'is_not'       => '不是',
+        'contains'     => '包含',
+        'contains_not' => '不包含',
+        'starts_with'  => '開始從',
+        'ends_with'    => '結束從',
+    ]
+];
